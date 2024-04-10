edbb706ecea572d33dec960c42f9fcc1aaa29e72
lozzaj <31875270+lozzaj@users.noreply.github.com>
2024-04-10 19:26:06 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,8 @@ if time_to_insert:
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
